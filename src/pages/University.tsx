acb636e6f73e5280bdf286eb0d8ce38144f93f51
Md. Meshbaul Haque Tamim <2102028@che.buet.ac.bd>
2025-10-01 23:21:44 +0600

    Add new pages for university services in the "More" section
    - Created AlumniNetwork.tsx for alumni connections.
    - Created BusSchedule.tsx for university transport schedules.
    - Created CampusMap.tsx for campus navigation.
    - Created CareerServices.tsx for job and internship resources.
    - Created ClassSchedule.tsx for viewing weekly class routines.
    - Created Classmates.tsx for connecting with classmates.
    - Created Clubs.tsx for discovering student communities.
    - Created CourseMaterials.tsx for accessing lecture notes and resources.
    - Created ELearning.tsx for online learning platforms.
    - Created FacultyDirectory.tsx for faculty contact information.
    - Created FinancialAid.tsx for scholarships and financial support.
    - Created HealthServices.tsx for accessing medical services.
    - Created ITSupport.tsx for IT assistance and troubleshooting.
    - Created LibraryPortal.tsx for accessing library resources.
    - Created Results.tsx for checking exam results and grades.
    - Created SportsFacilities.tsx for booking sports facilities.
    - Created StudentPortal.tsx for accessing student services.
    - Created UniversityNews.tsx for latest news and announcements.

--- src/pages/University.tsx
+++ src/pages/University.tsx
@@ -12,6 +12,26 @@ import UniversityNoticeBoard from "./University/NoticeBoard";
 import TeachersCorner from "./University/TeachersCorner";
 import NotFound from "./Fallbacks/NotFound";
 
+// More pages imports
+import CareerServices from "./University/More/CareerServices";
+import Clubs from "./University/More/Clubs";
+import CourseMaterials from "./University/More/CourseMaterials";
+import ClassSchedule from "./University/More/ClassSchedule";
+import Results from "./University/More/Results";
+import Classmates from "./University/More/Classmates";
+import LibraryPortal from "./University/More/LibraryPortal";
+import StudentPortal from "./University/More/StudentPortal";
+import FacultyDirectory from "./University/More/FacultyDirectory";
+import ELearning from "./University/More/ELearning";
+import UniversityNews from "./University/More/UniversityNews";
+import FinancialAid from "./University/More/FinancialAid";
+import AlumniNetwork from "./University/More/AlumniNetwork";
+import CampusMap from "./University/More/CampusMap";
+import SportsFacilities from "./University/More/SportsFacilities";
+import HealthServices from "./University/More/HealthServices";
+import BusSchedule from "./University/More/BusSchedule";
+import ITSupport from "./University/More/ITSupport";
+
 const University: React.FC = () => {
   return (
     <>
@@ -28,6 +48,27 @@ const University: React.FC = () => {
           <Route path="marketplace" element={<UniversityMarketplace />} />
           <Route path="teacherscorner" element={<TeachersCorner />} />
           <Route path="more" element={<UniversityMore />} />
+
+          {/* More section routes */}
+          <Route path="more/career-services" element={<CareerServices />} />
+          <Route path="more/clubs" element={<Clubs />} />
+          <Route path="more/course-materials" element={<CourseMaterials />} />
+          <Route path="more/class-schedule" element={<ClassSchedule />} />
+          <Route path="more/results" element={<Results />} />
+          <Route path="more/classmates" element={<Classmates />} />
+          <Route path="more/library-portal" element={<LibraryPortal />} />
+          <Route path="more/student-portal" element={<StudentPortal />} />
+          <Route path="more/faculty-directory" element={<FacultyDirectory />} />
+          <Route path="more/e-learning" element={<ELearning />} />
+          <Route path="more/university-news" element={<UniversityNews />} />
+          <Route path="more/financial-aid" element={<FinancialAid />} />
+          <Route path="more/alumni-network" element={<AlumniNetwork />} />
+          <Route path="more/campus-map" element={<CampusMap />} />
+          <Route path="more/sports-facilities" element={<SportsFacilities />} />
+          <Route path="more/health-services" element={<HealthServices />} />
+          <Route path="more/bus-schedule" element={<BusSchedule />} />
+          <Route path="more/it-support" element={<ITSupport />} />
+
           {/* Default route: show 404 not found */}
           <Route path="*" element={<NotFound />} />
         </Routes>
